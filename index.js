47939902ec348f15124a324370b7ad4768baf941
vreviglio <58663011+vreviglio@users.noreply.github.com>
2023-03-23 18:56:54 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -16,29 +16,10 @@ var hideChat = function hideChat() {
   var _window3;
   (_window3 = window) === null || _window3 === void 0 ? void 0 : _window3.zE('webWidget', 'hide');
 };
-var onInit = function onInit() {
-  var _window4;
-  (_window4 = window) === null || _window4 === void 0 ? void 0 : _window4.zE('webWidget:on', 'init', function () {
-    var _window5;
-    // Get the chat status on page load
-    var chatStatus = (_window5 = window) === null || _window5 === void 0 ? void 0 : _window5.zE('webWidget', 'chat:status');
-
-    // If there is an active chat, open it
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    if (chatStatus === 'active') {
-      var _window6;
-      (_window6 = window) === null || _window6 === void 0 ? void 0 : _window6.zE('webWidget', 'chat:show');
-    } else {
-      hideChat();
-    }
-  });
-};
 
 var zendeskActions = /*#__PURE__*/Object.freeze({
   __proto__: null,
   hideChat: hideChat,
-  onInit: onInit,
   openChat: openChat
 });
 
@@ -52,9 +33,9 @@ var initializeIframeListener = function initializeIframeListener() {
       console.warn("Invalid event or event data");
       return;
     }
-    var action = zendeskActions[data];
+    var action = zendeskActions[data.action];
     if (!action) {
-      console.warn("No action defined for ".concat(data));
+      console.warn("No action defined for ".concat(data.action));
       return;
     }
     action();
@@ -80,7 +61,9 @@ var injectZendeskScript = function injectZendeskScript() {
   var script = document.createElement('script');
   script.setAttribute('id', 'ze-snippet');
   script.setAttribute('src', "https://static.zdassets.com/ekr/snippet.js?key=".concat(key));
-  script.addEventListener('load', onInit);
+  script.addEventListener('load', function () {
+    return hideChat();
+  });
   document.body.appendChild(script);
 };
 
