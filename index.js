2f3b27cd42b75c20f2dbbfc25b53953f30add813
vreviglio <58663011+vreviglio@users.noreply.github.com>
2023-03-23 17:53:42 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 
 var onMessage = function onMessage(channelName, callback) {
   var channel = new BroadcastChannel(channelName);
-  channel.onmessage = function (event) {
-    console.log('RECEIVE MESSAGE');
+  channel.addEventListener('message', function (event) {
     callback(event.data);
-  };
+  });
 };
 
 var openChat = function openChat() {
@@ -17,10 +16,29 @@ var hideChat = function hideChat() {
   var _window3;
   (_window3 = window) === null || _window3 === void 0 ? void 0 : _window3.zE('webWidget', 'hide');
 };
+var onInit = function onInit() {
+  var _window4;
+  (_window4 = window) === null || _window4 === void 0 ? void 0 : _window4.zE('webWidget:on', 'init', function () {
+    var _window5;
+    // Get the chat status on page load
+    var chatStatus = (_window5 = window) === null || _window5 === void 0 ? void 0 : _window5.zE('webWidget', 'chat:status');
+
+    // If there is an active chat, open it
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    if (chatStatus === 'active') {
+      var _window6;
+      (_window6 = window) === null || _window6 === void 0 ? void 0 : _window6.zE('webWidget', 'chat:show');
+    } else {
+      hideChat();
+    }
+  });
+};
 
 var zendeskActions = /*#__PURE__*/Object.freeze({
   __proto__: null,
   hideChat: hideChat,
+  onInit: onInit,
   openChat: openChat
 });
 
@@ -62,19 +80,12 @@ var injectZendeskScript = function injectZendeskScript() {
   var script = document.createElement('script');
   script.setAttribute('id', 'ze-snippet');
   script.setAttribute('src', "https://static.zdassets.com/ekr/snippet.js?key=".concat(key));
-  script.addEventListener('load', function () {
-    return hideChat();
-  });
-  console.log('Appending child', script);
+  script.addEventListener('load', onInit);
   document.body.appendChild(script);
-  console.log('Child appended');
 };
 
 // Inject the Zendesk script tag into the target project
-console.log('Script Loaded');
-console.log('Starting injection');
 injectZendeskScript();
-console.log('Script injected');
 
 // Initialize the iframe listener
 initializeIframeListener();
