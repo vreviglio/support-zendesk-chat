5d4f86ac24edd24ad8f248f54793607046db0eb5
vreviglio <58663011+vreviglio@users.noreply.github.com>
2023-04-13 12:47:55 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,30 +7,48 @@ var onMessage = function onMessage(channelName, callback) {
   });
 };
 
-var openChat = function openChat(payload) {
-  var _window, _payload$tags, _window3, _window4;
-  var isChatting = (_window = window) === null || _window === void 0 ? void 0 : _window.zE('webWidget:get', 'chat:isChatting');
-
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  if (isChatting) {
-    return;
+var parsedPayloadToVisitorInfo = function parsedPayloadToVisitorInfo(payload) {
+  var visitorInfo = {};
+  if (!(payload !== null && payload !== void 0 && payload.visitorInfo)) return null;
+  var _payload$visitorInfo = payload.visitorInfo,
+    name = _payload$visitorInfo.name,
+    email = _payload$visitorInfo.email,
+    phone = _payload$visitorInfo.phone;
+  if (!name && !email && !phone) return null;
+  if (name) {
+    visitorInfo.name = name;
   }
+  if (email) {
+    visitorInfo.email = email;
+  }
+  if (phone) {
+    visitorInfo.phone = phone;
+  }
+  return visitorInfo;
+};
+
+var openChat = function openChat(payload) {
+  var _payload$tags, _window3, _window4, _window5;
   if (payload !== null && payload !== void 0 && (_payload$tags = payload.tags) !== null && _payload$tags !== void 0 && _payload$tags.length) {
+    var _window;
+    (_window = window) === null || _window === void 0 ? void 0 : _window.zE('webWidget', 'chat:addTags', payload === null || payload === void 0 ? void 0 : payload.tags);
+  }
+  var visitorInfo = parsedPayloadToVisitorInfo(payload);
+  if (visitorInfo) {
     var _window2;
-    (_window2 = window) === null || _window2 === void 0 ? void 0 : _window2.zE('webWidget', 'chat:addTags', payload === null || payload === void 0 ? void 0 : payload.tags);
+    (_window2 = window) === null || _window2 === void 0 ? void 0 : _window2.zE('webWidget', 'identify', visitorInfo);
   }
   (_window3 = window) === null || _window3 === void 0 ? void 0 : _window3.zE('webWidget', 'show');
   (_window4 = window) === null || _window4 === void 0 ? void 0 : _window4.zE('webWidget', 'open');
-};
-var hideChat = function hideChat() {
-  var _window5;
-  (_window5 = window) === null || _window5 === void 0 ? void 0 : _window5.zE('webWidget', 'hide');
+  (_window5 = window) === null || _window5 === void 0 ? void 0 : _window5.zE('webWidget', 'updateSettings', {
+    chat: {
+      suppress: false
+    }
+  });
 };
 
-var zendeskActions = /*#__PURE__*/Object.freeze({
+var chatActions = /*#__PURE__*/Object.freeze({
   __proto__: null,
-  hideChat: hideChat,
   openChat: openChat
 });
 
@@ -44,7 +62,7 @@ var initializeIframeListener = function initializeIframeListener() {
       console.warn("Invalid event or event data");
       return;
     }
-    var action = zendeskActions[data === null || data === void 0 ? void 0 : data.type];
+    var action = chatActions[data === null || data === void 0 ? void 0 : data.type];
     if (!action) {
       console.warn("No action defined for ".concat(data === null || data === void 0 ? void 0 : data.type));
       return;
@@ -61,32 +79,75 @@ var getQueryParamFromString = function getQueryParamFromString(scriptSrc, paramN
 
 /* eslint-disable no-console */
 
-// Injects the Zendesk script tag into the target project
-var injectZendeskScript = function injectZendeskScript() {
-  var _document;
-  var scriptSrc = ((_document = document) === null || _document === void 0 ? void 0 : _document.currentScript).src;
-  var key = getQueryParamFromString(scriptSrc, 'key');
-  if (!key) {
-    console.warn('Key was not provided');
-    return;
-  }
+// Creates the script element for the zendesk widget
+var createZendeskScriptElement = function createZendeskScriptElement(key) {
   var script = document.createElement('script');
   script.setAttribute('id', 'ze-snippet');
   script.setAttribute('src', "https://static.zdassets.com/ekr/snippet.js?key=".concat(key));
-  script.addEventListener('load', function () {
-    return hideChat();
-  });
-  document.body.appendChild(script);
+  return script;
 };
 
-console.log('A vel');
-try {
-  // Inject the Zendesk script tag into the target project
-  injectZendeskScript();
+// Sets the initial configuration for the zendesk chat
+var setZendeskChatConfiguration = function setZendeskChatConfiguration() {
+  window.zESettings = {
+    webWidget: {
+      launcher: {
+        chatLabel: {
+          '*': 'Help chat',
+          es: 'Chat de ayuda',
+          'es-es': 'Chat de ayuda',
+          'es-419': 'Chat de ayuda'
+        }
+      },
+      color: {
+        theme: '#7145D6'
+      },
+      chat: {
+        concierge: {
+          name: 'Live Support',
+          avatarPath: 'https://assets.cabifil.es/images/logos_cabify/lo_cabify_avatar.svg',
+          title: {
+            '*': 'Customer Support'
+          }
+        },
+        title: {
+          '*': 'Chat with us',
+          es: 'Chatea con nosotros',
+          'es-es': 'Chatea con nosotros',
+          'es-419': 'Chatea con nosotros'
+        },
+        hideWhenOffline: true,
+        suppress: true
+      }
+    }
+  };
+};
 
-  // Initialize the iframe listener
-  initializeIframeListener();
-} catch (error) {
-  console.log(error);
-}
+// Injects the Zendesk script tag into the target project
+var initializeZendeskIntegration = function initializeZendeskIntegration() {
+  var _document, _document$currentScri, _document2, _document2$body;
+  var scriptSrc = (_document = document) === null || _document === void 0 ? void 0 : (_document$currentScri = _document.currentScript) === null || _document$currentScri === void 0 ? void 0 : _document$currentScri.src;
+  if (!scriptSrc) {
+    console.error('Script src not found', {
+      severity: 'warning'
+    });
+    return;
+  }
+  var key = getQueryParamFromString(scriptSrc, 'key');
+  if (!key) {
+    console.error('Key was not provided', {
+      severity: 'warning'
+    });
+    return;
+  }
+  setZendeskChatConfiguration();
+  var script = createZendeskScriptElement(key);
+  (_document2 = document) === null || _document2 === void 0 ? void 0 : (_document2$body = _document2.body) === null || _document2$body === void 0 ? void 0 : _document2$body.appendChild(script);
+};
+
+// Set up zendeskConfiguration and inject the Zendesk script tag into the target project
+initializeZendeskIntegration();
+
+// Initialize the iframe listener
+initializeIframeListener();
 //# sourceMappingURL=index.js.map
